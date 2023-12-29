ffbbe424189ac0d7b499761309523f17771c6dfe
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2023-12-29 02:49:02 +0000

    Create model1.py

---
+++ Models/model1.py
@@ -0,0 +1,142 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+
+            
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(mid_channels),
+            nn.LeakyReLU(negative_slope=0.01 , inplace=True),
+            
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(negative_slope=0.01 , inplace=True),
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+class DoubleConv_s2(nn.Module):
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1,stride=2),
+            nn.BatchNorm2d(mid_channels),
+            nn.LeakyReLU(negative_slope=0.01 , inplace=True),
+            
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(negative_slope=0.01 , inplace=True),
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+    
+    
+class Down(nn.Module):
+    """Downscaling with maxpool then double conv"""
+
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv_s2 = nn.Sequential(
+            DoubleConv_s2(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.conv_s2(x)
+
+class Up(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+
+        # if bilinear, use the normal convolutions to reduce the number of channels
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+            self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        x = torch.cat([x2, x1], dim=1)
+        
+        return self.conv(x)
+
+class Up_last1(nn.Module):
+    """Upscaling then double conv"""
+
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+
+        # if bilinear, use the normal convolutions to reduce the number of channels
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+        else:
+            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=1, padding=0)
+            self.conv = DoubleConv(96, out_channels)
+
+    def forward(self, x1, x2):
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+    
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+class UNet5_4_M_CONV(nn.Module):
+    def __init__(self, n_channels = 1, bilinear=False):
+        super(UNet5_4_M_CONV, self).__init__()
+        self.n_channels = n_channels
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(n_channels, 32)
+        self.down1 = Down(32, 64)
+        self.down2 = Down(64, 128)
+        self.down3 = Down(128, 256)
+        self.down4 = Down(256, 512)
+
+        
+    def forward(self, x):
+
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)        
+        
+        return x5
+    
+Input_Image_Channels = 1
+def model() -> UNet5_4_M_CONV:
+    model = UNet5_4_M_CONV()
+    return model
+DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+from torchsummary import summary
+model = model()
+model.to(device=DEVICE,dtype=torch.float)
+summary(model, [(Input_Image_Channels, 256,256)])
