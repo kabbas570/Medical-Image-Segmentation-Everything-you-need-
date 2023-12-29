9fad6482871ae178a975d659dddc8a5b555885f9
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2023-12-29 14:23:57 +0000

    Update model1.py

--- Models/model1.py
+++ Models/model1.py
@@ -1,11 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torchvision
-
             
 class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels, mid_channels=None):
@@ -45,8 +39,6 @@ class DoubleConv_s2(nn.Module):
     
     
 class Down(nn.Module):
-    """Downscaling with maxpool then double conv"""
-
     def __init__(self, in_channels, out_channels):
         super().__init__()
         self.conv_s2 = nn.Sequential(
@@ -57,46 +49,14 @@ class Down(nn.Module):
         return self.conv_s2(x)
 
 class Up(nn.Module):
-    """Upscaling then double conv"""
-
-    def __init__(self, in_channels, out_channels, bilinear=True):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
 
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
-        else:
-            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-            self.conv = DoubleConv(in_channels, out_channels)
+        self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+        self.conv = DoubleConv(in_channels, out_channels)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
-        # input is CHW
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
-
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2])
-        x = torch.cat([x2, x1], dim=1)
-        
-        return self.conv(x)
-
-class Up_last1(nn.Module):
-    """Upscaling then double conv"""
-
-    def __init__(self, in_channels, out_channels, bilinear=True):
-        super().__init__()
-
-        # if bilinear, use the normal convolutions to reduce the number of channels
-        if bilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
-        else:
-            self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=1, padding=0)
-            self.conv = DoubleConv(96, out_channels)
-
-    def forward(self, x1, x2):
         x = torch.cat([x2, x1], dim=1)
         return self.conv(x)
     
@@ -108,18 +68,25 @@ class OutConv(nn.Module):
     def forward(self, x):
         return self.conv(x)
 
-class UNet5_4_M_CONV(nn.Module):
-    def __init__(self, n_channels = 1, bilinear=False):
-        super(UNet5_4_M_CONV, self).__init__()
+class BaseLine1(nn.Module):
+    def __init__(self, n_channels = 1):
+        super(BaseLine1, self).__init__()
         self.n_channels = n_channels
-        self.bilinear = bilinear
 
         self.inc = DoubleConv(n_channels, 32)
         self.down1 = Down(32, 64)
         self.down2 = Down(64, 128)
         self.down3 = Down(128, 256)
         self.down4 = Down(256, 512)
-
+        self.down5 = Down(512, 1024)
+        
+        self.up0 = Up(1024, 512)
+        self.up1 = Up(512, 256)
+        self.up2 = Up(256, 128)
+        self.up3 = Up(128, 64)
+        self.up4 = Up(64, 32)
+        
+        self.outc = OutConv(32,4)
         
     def forward(self, x):
 
@@ -127,16 +94,25 @@ class UNet5_4_M_CONV(nn.Module):
         x2 = self.down1(x1)
         x3 = self.down2(x2)
         x4 = self.down3(x3)
-        x5 = self.down4(x4)        
+        x5 = self.down4(x4)  
+        x6 = self.down5(x5) 
         
-        return x5
+        z1 = self.up0(x6, x5)
+        z2 = self.up1(z1, x4)
+        z3 = self.up2(z2, x3)
+        z4 = self.up3(z3, x2)
+        z5 = self.up4(z4, x1)
+
+        logits1 = self.outc(z5)
+        
+        return logits1
     
-Input_Image_Channels = 1
-def model() -> UNet5_4_M_CONV:
-    model = UNet5_4_M_CONV()
-    return model
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-from torchsummary import summary
-model = model()
-model.to(device=DEVICE,dtype=torch.float)
-summary(model, [(Input_Image_Channels, 256,256)])
+# Input_Image_Channels = 1
+# def model() -> BaseLine1:
+#     model = BaseLine1()
+#     return model
+# DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+# from torchsummary import summary
+# model = model()
+# model.to(device=DEVICE,dtype=torch.float)
+# summary(model, [(Input_Image_Channels, 256,256)])
