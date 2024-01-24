2ea8fd4d71d4f8324faa97c4bcd071731f0859f2
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2024-01-24 17:58:28 +0000

    Update calculate_SJ_Distance.py

--- pre-processing-Files/calculate_SJ_Distance.py
+++ pre-processing-Files/calculate_SJ_Distance.py
@@ -3,6 +3,8 @@ import SimpleITK as sitk
 import numpy as np
 import matplotlib.pyplot as plt
 import glob
+from scipy.spatial import distance
+from scipy.stats import entropy
 
 DIM_ = 256
 
@@ -48,31 +50,36 @@ def Normalization_2(img):
         img=(img-mean)/std
         return img 
 
-def normalize_1(img):
+def Normalization_1(img):
  return (img - 125.667 ) / 208.77
+
+bins = 'fd'
 
 def cal_SJ(img_paths):
     summ_all = []
     for i in range(10):
         image1 =  sitk.GetArrayFromImage(sitk.ReadImage(img_paths[i]))
-        image1 = normalize_1(image1)
-        hist1, _ = np.histogram(image1, bins=256, range=(0,600), density=True)
+        #image1 = Normalization_2(image1)
+        hist1, bin_edges  = np.histogram(image1, bins=bins, density=True)
 
         for j in range(10):  
             if j!=i:
                 image2 =  sitk.GetArrayFromImage(sitk.ReadImage(img_paths[j]))
-                image2 = normalize_1(image2)
-                hist2, _ = np.histogram(image2, bins=256, range=(0,600), density=True)
+                #image2 = Normalization_2(image2)
+                hist2, _ = np.histogram(image2, bins=bin_edges,  density=True)
+
                 js_distance = jensenshannon(hist1, hist2)
                 summ_all.append(js_distance)
+                
+
     
-    return np.mean(summ_all)
+    return np.mean(summ_all),image1
             
             
 if __name__ == "__main__":
     path_to_imgs = r'C:\My_Data\M2M Data\data\data_2\three_vendor\all\LA\imgs/'
     img_paths = sorted(glob.glob(path_to_imgs + '*.nii.gz'))
 
-    avg_js_dist_1= cal_SJ(img_paths)
+    avg_js_dist_1,image1  = cal_SJ(img_paths)
 
     print('Average JS Distance =', avg_js_dist_1)
