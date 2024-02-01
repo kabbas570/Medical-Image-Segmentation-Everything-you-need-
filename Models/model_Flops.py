80c149948d3295ba5aeb0b9ba7f2bf2966c001a1
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2024-02-01 10:38:43 +0000

    Create model_Flops.py

---
+++ Models/model_Flops.py
@@ -0,0 +1,126 @@
+import torch
+import torch.nn as nn    
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(mid_channels),
+            nn.LeakyReLU(negative_slope=0.01 , inplace=True),
+            
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(negative_slope=0.01 , inplace=True),
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+
+class DoubleConv_s2(nn.Module):
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1,stride=2),
+            nn.BatchNorm2d(mid_channels),
+            nn.LeakyReLU(negative_slope=0.01 , inplace=True),
+            
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(negative_slope=0.01 , inplace=True),
+        )
+
+    def forward(self, x):
+        return self.double_conv(x)
+    
+    
+class Down(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.conv_s2 = nn.Sequential(
+            DoubleConv_s2(in_channels, out_channels)
+        )
+
+    def forward(self, x):
+        return self.conv_s2(x)
+
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+
+        self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+        self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+    
+class OutConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConv, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
+
+class BaseLine1(nn.Module):
+    def __init__(self, n_channels = 1):
+        super(BaseLine1, self).__init__()
+        self.n_channels = n_channels
+
+        self.inc = DoubleConv(n_channels, 32)
+        self.down1 = Down(32, 64)
+        self.down2 = Down(64, 128)
+        self.down3 = Down(128, 256)
+        self.down4 = Down(256, 512)
+        self.down5 = Down(512, 1024)
+        
+        self.up0 = Up(1024, 512)
+        self.up1 = Up(512, 256)
+        self.up2 = Up(256, 128)
+        self.up3 = Up(128, 64)
+        self.up4 = Up(64, 32)
+        
+        self.outc = OutConv(32,4)
+        
+    def forward(self, x):
+
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)  
+        x6 = self.down5(x5) 
+        
+        z1 = self.up0(x6, x5)
+        z2 = self.up1(z1, x4)
+        z3 = self.up2(z2, x3)
+        z4 = self.up3(z3, x2)
+        z5 = self.up4(z4, x1)
+
+        logits1 = self.outc(z5)
+        
+        return logits1
+    
+# Input_Image_Channels = 1
+# def model() -> BaseLine1:
+#     model = BaseLine1()
+#     return model
+# DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+# from torchsummary import summary
+# model = model()
+# model.to(device=DEVICE,dtype=torch.float)
+# summary(model, [(Input_Image_Channels, 384,384)])
+
+from ptflops import get_model_complexity_info
+net = BaseLine1()
+flops, params = get_model_complexity_info(net, (1,128, 128), as_strings=True,
+                                            print_per_layer_stat=True, verbose=True)
+print('{:<30}  {:<8}'.format('Computational complexity: ', flops))
+print('{:<30}  {:<8}'.format('Number of parameters: ', params))
+
+
