106221b97cc021dd499fb080a8711c7bb24b1dac
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2024-02-06 17:36:55 +0000

    Update LA-SegNet_WO_HLC.py

--- Models/LA-SegNet_WO_HLC.py
+++ Models/LA-SegNet_WO_HLC.py
@@ -67,26 +67,24 @@ class Down(nn.Module):
 class Up(nn.Module):
     def __init__(self, in_channels, out_channels,increase_channels = True):
         super().__init__()
-        
-        print('in_channels --> ',in_channels)
-        
+                
         if increase_channels:
             if (in_channels%out_channels) ==0:
                 mid_channel = in_channels
             else:
+                print(in_channels, '   ', out_channels)
                 mid_channel = in_channels + 96
         else:
             mid_channel = in_channels + in_channels//2
                 
+        print('mid_channel --<>' , mid_channel)
         self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-        print('mid_channel --> ',mid_channel)
         self.conv = DoubleConv(mid_channel , out_channels)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
         x = torch.cat([x2, x1], dim=1)
         
-        print(x.shape)
         
         return self.conv(x)
     
@@ -98,7 +96,7 @@ class OutConv(nn.Module):
     def forward(self, x):
         return self.conv(x)
 
-Base = 32
+Base = 16
 class BaseLine1(nn.Module):
     def __init__(self, n_channels = 1):
         super(BaseLine1, self).__init__()
@@ -108,11 +106,14 @@ class BaseLine1(nn.Module):
         self.down1 = Down(Base, 2*Base)
         self.down2 = Down(2*Base, 4*Base)
         self.down3 = Down(4*Base, 8*Base)
-        self.down4 = Down(8*Base, 10*Base)
-        self.down5 = Down(10*Base, 10*Base)
+        self.down4 = Down(8*Base, 16*Base)
+        self.down5 = Down(16*Base, 20*Base)
+        self.down6 = Down(20*Base, 20*Base)
         
-        self.up0 = Up(10*Base, 10*Base,False)
-        self.up1 = Up(10*Base, 8*Base)
+        
+        self.up00 = Up(20*Base, 20*Base,False)
+        self.up0 = Up(20*Base, 16*Base)
+        self.up1 = Up(16*Base, 8*Base)
         self.up2 = Up(8*Base, 4*Base)
         self.up3 = Up(4*Base, 2*Base)
         self.up4 = Up(2*Base, Base)
@@ -129,9 +130,11 @@ class BaseLine1(nn.Module):
         x4 = self.down3(x3)
         x5 = self.down4(x4) 
         x6 = self.down5(x5) 
-        x6 = self.dropout6E(x6)
+        x7 = self.down6(x6) 
+        x7 = self.dropout6E(x7)
 
-        z1 = self.up0(x6, x5)
+        z0 = self.up00(x7,x6)
+        z1 = self.up0(z0, x5)
         z2 = self.up1(z1, x4)
         z3 = self.up2(z2, x3)
         z4 = self.up3(z3, x2)
@@ -148,11 +151,11 @@ class BaseLine1(nn.Module):
 # from torchsummary import summary
 # model = model()
 # model.to(device=DEVICE,dtype=torch.float)
-# summary(model, [(Input_Image_Channels, 384,320)])
+# summary(model, [(Input_Image_Channels, 384,384)])
 
 from ptflops import get_model_complexity_info
 net = BaseLine1()
-flops, params = get_model_complexity_info(net, (1,384,320), as_strings=True,
+flops, params = get_model_complexity_info(net, (1,384,384), as_strings=True,
                                             print_per_layer_stat=True, verbose=True)
 print('{:<30}  {:<8}'.format('Computational complexity: ', flops))
 print('{:<30}  {:<8}'.format('Number of parameters: ', params))
