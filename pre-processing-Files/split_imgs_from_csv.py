6d25a5ce49506f3490a0e9e2c83ef904a9486ac4
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2024-02-07 15:38:32 +0000

    Update split_imgs_from_csv.py

--- pre-processing-Files/split_imgs_from_csv.py
+++ pre-processing-Files/split_imgs_from_csv.py
@@ -2,27 +2,31 @@ import pandas as pd
 import os
 import shutil
 
-csv_1 = r"C:\My_Data\M2M Data\data\data_2\five_fold\sep\new_split_mix\F1\train\F1_train.csv"
+
+source_folder_img = r'C:\My_Data\M2M Data\data\data_2\five_fold\sep\LA_Data\all_train\imgs'
+source_folder_gt = r'C:\My_Data\M2M Data\data\data_2\five_fold\sep\LA_Data\all_train\gts'
+
+
+csv_1 = r"C:\My_Data\M2M Data\data\data_2\five_fold\sep\new_split_mix\F2\val\F2_val.csv"
+destination_folder_img = r'C:\My_Data\M2M Data\data\data_2\five_fold\sep\new_split_mix\F2\val\imgs'
+destination_folder_gt = r'C:\My_Data\M2M Data\data\data_2\five_fold\sep\new_split_mix\F2\val\gts'
+
+
 df1 = pd.read_csv(csv_1)
 n = df1['SUBJECT_CODE']
 
-file_names_to_copy = []
+img_file_names_to_copy = []
 for i in n:
     k = str(i).zfill(3)
-    print(k)
     k1 = k + '_LA_ED.nii.gz'
     k2 = k + '_LA_ES.nii.gz'
-    file_names_to_copy.append(k1)
-    file_names_to_copy.append(k2)
+    img_file_names_to_copy.append(k1)
+    img_file_names_to_copy.append(k2)
     
-
-source_folder = r'C:\My_Data\M2M Data\data\data_2\five_fold\sep\LA_Data\all_train\imgs'
-destination_folder = r'C:\My_Data\M2M Data\data\data_2\five_fold\sep\new_split_mix\F1\train\imgs'
-os.makedirs(destination_folder, exist_ok=True)
-
-for file_name in file_names_to_copy:
-    source_path = os.path.join(source_folder, file_name)
-    destination_path = os.path.join(destination_folder, file_name)
+os.makedirs(destination_folder_img, exist_ok=True)
+for file_name in img_file_names_to_copy:
+    source_path = os.path.join(source_folder_img, file_name)
+    destination_path = os.path.join(destination_folder_img, file_name)
     
     # Check if the file exists before copying
     if os.path.isfile(source_path):
@@ -34,23 +38,23 @@ for file_name in file_names_to_copy:
 print("Copying completed.")
 
 
-file_names_to_copy = []
+
+
+gt_file_names_to_copy = []
 for i in n:
     k = str(i).zfill(3)
     print(k)
     k1 = k + '_LA_ED_gt.nii.gz'
     k2 = k + '_LA_ES_gt.nii.gz'
-    file_names_to_copy.append(k1)
-    file_names_to_copy.append(k2)
+    gt_file_names_to_copy.append(k1)
+    gt_file_names_to_copy.append(k2)
     
+    
+os.makedirs(destination_folder_gt, exist_ok=True)
 
-source_folder = r'C:\My_Data\M2M Data\data\data_2\five_fold\sep\LA_Data\all_train\gts'
-destination_folder = r'C:\My_Data\M2M Data\data\data_2\five_fold\sep\new_split_mix\F1\train\gts'
-os.makedirs(destination_folder, exist_ok=True)
-
-for file_name in file_names_to_copy:
-    source_path = os.path.join(source_folder, file_name)
-    destination_path = os.path.join(destination_folder, file_name)
+for file_name in gt_file_names_to_copy:
+    source_path = os.path.join(source_folder_gt, file_name)
+    destination_path = os.path.join(destination_folder_gt, file_name)
     
     # Check if the file exists before copying
     if os.path.isfile(source_path):
