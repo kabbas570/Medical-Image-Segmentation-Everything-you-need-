617cfa60fcfeafe3443bcd68e8512a56f1ab91cd
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2024-03-14 12:51:35 +0000

    Create LR_Scheduler.py

---
+++ Training/LR_Scheduler.py
@@ -0,0 +1,11 @@
+import torch
+from torch import autograd, optim
+
+x = autograd.Variable(torch.FloatTensor([1.0]), requires_grad=True)
+optimizer = optim.SGD([x], lr=1)
+
+scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=[10,50,80,100], gamma=0.1)
+for epoch in range(100):
+    scheduler.step()
+    optimizer.step()
+    print(epoch, '    ',scheduler.get_last_lr())
