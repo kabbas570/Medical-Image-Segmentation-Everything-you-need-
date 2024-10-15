88b0d28b0907922297ec99752a377dd2e32eadf9
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2024-10-15 15:37:27 +0100

    Update Feature_Reduction1.py

--- KD/Feature_Reduction1.py
+++ KD/Feature_Reduction1.py
@@ -3,7 +3,7 @@ def pool_spatial(tensor):
     pooled = torch.max(tensor, dim=1, keepdim=True)[0] ## or Change to Avg
     return pooled
 
-def reducing_1(tensor,Teacher_Dim,Student_Dim):
+def reducing_1(teacher_tensor,Teacher_Dim,Student_Dim):
     Factor = Teacher_Dim//Student_Dim
     pooled_tensors = []
     splitted_tensors = torch.split(teacher_tensor, split_size_or_sections=Factor, dim=1)
