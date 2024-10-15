af01dcb20e95d8d18da960150c6f8869dc024545
Abbas Khan <56618776+kabbas570@users.noreply.github.com>
2024-10-15 14:59:08 +0100

    Update Feature_Reduction1.py

--- KD/Feature_Reduction1.py
+++ KD/Feature_Reduction1.py
@@ -1,20 +1,18 @@
 import torch
 
-Teacher_Dim = 24
-Student_Dim = 6
-Factor = Teacher_Dim//Student_Dim
-original_tensor = torch.randn(2, Teacher_Dim, 16, 16)
-t1, t2 ,t3, t4, t5, t6 = torch.split(original_tensor, split_size_or_sections=Factor, dim=1)
 
-# Print the shapes to verify
-print("Original tensor shape:", original_tensor.shape)
-print("t1 shape:", t1.shape)
-print("t2 shape:", t2.shape)
-
-# Function to pool along spatial dimensions
 def pool_spatial(tensor):
     pooled = torch.max(tensor, dim=1, keepdim=True)[0] ## or Change to Avg
     return pooled
 
-pooled_t = torch.cat([pool_spatial(t1),pool_spatial(t2),pool_spatial(t3),pool_spatial(t4),pool_spatial(t5),pool_spatial(t6)],1)
+def reducing_1(tensor,Teacher_Dim,Student_Dim):
+    Factor = Teacher_Dim//Student_Dim
+    t1, t2 ,t3, t4, t5, t6 = torch.split(tensor, split_size_or_sections=Factor, dim=1)
+    pooled_t = torch.cat([pool_spatial(t1),pool_spatial(t2),pool_spatial(t3),pool_spatial(t4),pool_spatial(t5),pool_spatial(t6)],1)
+    return pooled_t
 
+Teacher_Dim = 24
+Student_Dim = 6
+
+teacher_tensor = torch.randn(2, Teacher_Dim, 16, 16)
+pooled_t = reducing_1(teacher_tensor,Teacher_Dim,Student_Dim)
